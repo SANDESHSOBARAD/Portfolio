b8feb192171fe39845fc7d3c5a925ec032b138f4
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2023-04-17 12:51:30 -0700

    backend schema created and front end header done

---
+++ frontend/src/container/About/About.jsx
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import './About.scss';
+import {motion} from 'framer-motion';
+
+
+
+const About = () => {
+  return (
+    <>
+    <h2 className='head-text'>Code with
+    <span> Passion</span><br/>
+    Create with
+    <span> Purpose!</span>
+    </h2>
+    <div className='app__profile'>
+
+    </div>
+    </>
+  )
+}
+
+export default About
---
+++ sanity_backend/schemas/index.js
@@ -0,0 +1,8 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+export const schemaTypes = [testimonials,abouts,contact,experiences,skills,workExperience,works]
